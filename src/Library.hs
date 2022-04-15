96da414703dc0c1b80efb6dfdc0ecd2a91dea723
Guadalupe <gabdelnour@frba.utn.edu.ar>
2022-04-15 20:20:50 -0300

    TPindividual 1

---
+++ src/Library.hs
@@ -0,0 +1,42 @@
+module Library where
+import PdePreludat
+
+doble :: Number -> Number
+doble x = 2 * x
+
+
+data Grupo = UnGrupo String Number [Number] deriving Show
+getListaAcomp (UnGrupo _ _ listaAcomp) = listaAcomp
+getEdadEncargado (UnGrupo _ edadEncargado _) = edadEncargado
+getNombreEncargado (UnGrupo nombreEncargado _ _) = nombreEncargado
+
+
+-- datos de grupos
+grupoA :: Grupo 
+grupoA = UnGrupo "Pedro" 20 [18, 16]
+
+grupoB :: Grupo 
+grupoB = UnGrupo "Leo" 53 [20, 20, 18]
+
+grupoC :: Grupo 
+grupoC = UnGrupo "Maria" 30 []
+
+grupoD :: Grupo 
+grupoD = UnGrupo "Juan" 80 [15,20,40]
+
+
+
+-- FUNCIONES
+contarIntegrantes ::Grupo -> Number 
+contarIntegrantes grupo = (length (getListaAcomp grupo)) + 1
+
+sumaEdadesGrupo :: Grupo -> Number
+sumaEdadesGrupo grupo = (sum (getListaAcomp grupo) + getEdadEncargado grupo)
+
+promedioEdadGrupo :: Grupo -> Number
+promedioEdadGrupo grupo = sumaEdadesGrupo grupo /contarIntegrantes grupo
+
+grupoMayorPromEdad :: Grupo -> Grupo -> String
+grupoMayorPromEdad grupo1 grupo2 |(promedioEdadGrupo grupo1 > promedioEdadGrupo grupo2) = getNombreEncargado grupo1
+                                 | (promedioEdadGrupo grupo2 > promedioEdadGrupo grupo1) = getNombreEncargado grupo2
+
